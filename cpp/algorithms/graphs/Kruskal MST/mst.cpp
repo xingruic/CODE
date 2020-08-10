f0b9c85558d51e75e509059b61b7d8f2197ec5df
NevGithub0823 <xingruichen2@gmail.com>
2020-08-09 22:34:58 -0700

    Update Kruskal MST

---
+++ cpp/algorithms/graphs/Kruskal MST/mst.cpp
@@ -0,0 +1,54 @@
+// oj.01class.cn/problem/30001
+#include<bits/stdc++.h>
+using namespace std;
+struct edge{
+    // from,to,weight
+    int f,t,w;
+};
+int father[20001];
+int size[20001]={0};
+bool cmp(edge a,edge b){return a.w<b.w;}
+int root(int a){
+    if(father[a]==a) return a;
+    return father[a]=root(father[a]);
+}
+void join(int a,int b){
+    int x=root(a);
+    int y=root(b);
+    if(x!=y) father[x]=y;
+}
+int main(){
+    freopen("30001.in","r",stdin);
+    freopen("30001.out","w",stdout);
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++) father[i]=i;
+    int b[n][n];
+    for(int i=0; i<n; i++) for(int j=0; j<n; j++) cin>>b[i][j];
+    int cnt=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cnt++;
+        }
+    }
+    edge a[cnt];
+    cnt=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            a[cnt].f=i;
+            a[cnt].t=j;
+            a[cnt++].w=b[i][j];
+        }
+    }
+    sort(a,a+cnt,cmp);
+    int ans=0;
+    for(int i=0; i<cnt; i++){
+        // cout<<root(a[i].f)<<' '<<root(a[i].t)<<' '<<a[i].w<<endl;
+        if(root(a[i].f)!=root(a[i].t)){
+            join(a[i].f,a[i].t);
+            ans+=a[i].w;
+        }
+        if(ans==n-1) break;
+    }
+    cout<<ans<<endl;
+}
