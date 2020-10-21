be5ee7e51069119d10b36511a57bb7fd7cdbcd50
Xingrui Chen <xingruichen2@gmail.com>
2020-10-21 08:16:14 -0700

    39054

---
+++ cpp/oj.01class.cn/39054/39054.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string k,text;
+    cin>>k>>text;
+    char a['Z'+5]['Z'+5];
+    for(int i='A'; i<='Z'; i++){
+        for(int j='A'; j<='Z'; j++){
+            if(j+i-'A'<='Z') a[i][j]=(char)(j+i-'A');
+            else a[i][j]=(char)(j+i-'A'-26);
+            // cout<<a[i][j]<<' ';
+        }// cout<<endl;
+    }
+    for(int i=0; i<k.size(); i++){
+        if(k[i]>'Z') k[i]-='a'-'A';
+        if(k[i]=='A') k[i]='[';
+    }
+    // cout<<k<<endl;
+    string key="";
+    for(int i=0,j=0; i<text.size(); i++){
+        if(j==k.size()) j=0;
+        key+=k[j];
+        j++;
+    }
+    // cout<<key<<endl;
+    for(int i=0; i<text.size(); i++){
+        // cout<<text[i]<<' '<<(char)('A'+'Z'+1-key[i])<<' ';
+        if(text[i]<='Z'){
+            text[i]=a[text[i]]['A'+'Z'+1-key[i]];
+        }else{
+            text[i]=a[text[i]-'a'+'A']['A'+'Z'+1-key[i]]-'A'+'a';
+        }
+        // cout<<text[i]<<endl;
+    }
+    cout<<text<<endl;
+}
