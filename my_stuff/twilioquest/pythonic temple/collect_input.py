e63ed4969d4362f7d125eab34e4e70062ccab916
NevGithub0823 <xingruichen2@gmail.com>
2021-01-08 13:11:01 -0800

    my_stuff got deleted

--- my_stuff/twilioquest/pythonic temple/collect_input.py
+++
@@ -1,2 +0,0 @@
-import sys
-print(sys.argv[1]+'\n'+sys.argv[2]+'\n'+sys.argv[3])
--- my_stuff/twilioquest/pythonic temple/dictionaries.py
+++
@@ -1,4 +0,0 @@
-super_hero = dict()
-super_hero["name"] = "Miles Morales"
-super_hero["hero_name"] = "Spider-Man"
-super_hero["power_level"] = 9999
