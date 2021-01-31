19e044f4eca28192d33b84d3d310d655eec76e9b
Bootcamp Windows <xingruichen2@gmail.com>
2021-01-30 18:42:12 -0800

    add testing.cpp

--- cpp/testing.cpp
+++ cpp/testing.cpp
@@ -1,15 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int ip=0; // insertion point = ip, to mark where to insert characters
-void append(char ans[],string str){
-    int stop=ip+str.size();
-    for(int i=ip; i<stop; i++){
-        ans[i]=str[i-ip];
-    }
-    return;
-}
 int main(){
-	char ans[100]={0};
-    append(ans,"hello");
-    for(int i=0; i<10; i++) cout<<ans[i]<<endl;
+    cout<<11.1%1<<endl;
 }
