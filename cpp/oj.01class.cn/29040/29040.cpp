d9729dd5e1b42860858f6e87ed6c39fa5e098dac
NevGithub0823 <xingruichen2@gmail.com>
2021-03-16 18:22:39 -0700

    Commit 3/16 6:22 PM

--- cpp/oj.01class.cn/29040/29040.cpp
+++ cpp/oj.01class.cn/29040/29040.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,mars[10000],add;
 int main(){
+    int n,add;
     cin>>n>>add;
+    int mars[n];
     for(int i=0; i<n; i++){
         cin>>mars[i];
     }
@@ -23,20 +24,25 @@ int main(){
         // cout<<(multiplier<0?0:multiplier)*fact[n-i-1]<<'\t';
     }
     translated+=add; // add the given number to the translated number
-    cout<<translated<<endl;
+    // cout<<translated<<endl;
     string result="";
-    vector<int> a;
-    a.reserve(n);
-    for(int i=0; i<n; i++) a[i]=i+1;
-    for(int i=4; i>=0; i--){
-        translated%=fact[i];
-        cout<<translated<<' '<<fact[i]<<' ';
-        result=(char)(a[translated]+'0')+result;
-        vector<int>::iterator itr=a.begin();
-        for(int j=0; j<translated-1; j++){
-            itr++;
-        }
-        a.erase(itr);
+    int a[n];
+    for(int i=0; i<n; i++){
+        a[i]=i+1;
+        // cout<<a[i]<<endl;
     }
+    for(int i=n-1; i>=0; i--){
+        int t=translated/fact[i];
+        for(int j=0; j<=t; j++){
+            if(a[j]==-1) t++;
+        }
+        assert(t<n);
+        // cout<<endl<<t<<' '<<translated<<' '<<a[t]<<endl;
+        assert(a[t]>0);
+        result=result+(char)(a[t]+'0')+' ';
+        translated%=fact[i];
+        a[t]=-1;
+    }
+    result.pop_back();
     cout<<result<<endl;
 }
