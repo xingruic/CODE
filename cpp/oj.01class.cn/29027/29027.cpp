3514b9a792520750b7066c884589051269e8be6c
Bootcamp Windows <xingruichen2@gmail.com>
2021-06-18 11:19:07 -0700

    29027

--- cpp/oj.01class.cn/29027/29027.cpp
+++ cpp/oj.01class.cn/29027/29027.cpp
@@ -1,28 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-string in,post,pre;
-void inpopre(int st,int end,int sst,int endd){
-  if(st==end){
-      cout<<"in:"<<in[end]<<",at"<<end<<endl;
-  }
-  if(sst-1==endd){
-      cout<<"post:"<<post[endd]<<",at"<<endd<<endl;
-  }
-  if(st==end||sst-1==endd) return;
-  pre=pre+post[endd];
-  cout<<endl<<pre<<endl;
-  for(int i=st; i<end; i++){
-    if(in[i]==post[endd]){
-      inpopre(st,i,sst,sst+i-st-1);
-      inpopre(i+1,end,sst+i-st,endd-1);
+string In, Post;
+// We first push right subtree to a stack, then left subtree, and finally, we push root. Finally, we print contents of stack.
+stack<char> pre;
+void push_pre(string in, string post, char lbl) {
+    int len = in.size();
+    if (len == 0) return;
+
+    int i;
+    for (i = 0; i < len; i++) if (in[i] == lbl) break;
+
+    string next_in = in.substr(i + 1, len - i - 1);
+    string next_post = post.substr(i, len - i - 1);
+    push_pre(next_in, next_post, next_post[next_post.size() - 1]);
+
+    next_in = in.substr(0, i);
+    next_post = post.substr(0, i);
+    push_pre(next_in, next_post, next_post[next_post.size() - 1]);
+
+    pre.push(post[len - 1]);
+
+    return;
+}
+void print_pre() {
+    while (!pre.empty()) {
+        cout << pre.top();
+        pre.pop();
     }
-  }
+    return;
 }
-int main(){
-  cin>>in>>post;
-  inpopre(0,in.size()-1,0,post.size()-1);
-  cout<<pre<<endl;
+int main() {
+    cin >> In >> Post;
+    push_pre(In, Post, Post[Post.size() - 1]);
+    print_pre();
 }
-// BADC  BDCA
-// CBDAEGF CDBGFEA
-// SHAGBF SHGFBA
+// BADC BDCA
