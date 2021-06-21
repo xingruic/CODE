0bb8a20f9bee1357c806bec3a98ddc176d7f5bbf
Bootcamp Windows <xingruichen2@gmail.com>
2021-06-20 21:04:03 -0700

    t i n y d e t a i l

--- cpp/train.usaco.org/ratios/ratios.cpp
+++ cpp/train.usaco.org/ratios/ratios.cpp
@@ -5,35 +5,35 @@ TASK:ratios
 */
 #include<bits/stdc++.h>
 using namespace std;
-struct triple{
-    int a=-1,b=-1,c=-1;
-    bool operator =(triple one,triple two){
-        return one.a==two.a&&one.b==two.b&&one.c==two.c;
+struct triple {
+    int a = -1, b = -1, c = -1;
+    bool operator ==(triple one, triple two) {
+        return one.a == two.a && one.b == two.b && one.c == two.c;
     }
-    triple operator *(int one,triple two){
+    triple operator *(int one, triple two) {
         triple three;
-        three.a=one*two.a;
-        three.b=one*two.b;
-        three.c=one*two.c;
+        three.a = one * two.a;
+        three.b = one * two.b;
+        three.c = one * two.c;
         return three;
     }
-    triple operator *(triple two,int one){
+    triple operator *(triple two, int one) {
         triple three;
-        three.a=one*two.a;
-        three.b=one*two.b;
-        three.c=one*two.c;
+        three.a = one * two.a;
+        three.b = one * two.b;
+        three.c = one * two.c;
         return three;
     }
-    triple operator +(triple one,triple two){
+    triple operator +(triple one, triple two) {
         triple three;
-        three.a=one.a+two.a;
-        three.b=one.b+two.b;
-        three.c=ine.c+two.c;
+        three.a = one.a + two.a;
+        three.b = one.b + two.b;
+        three.c = ine.c + two.c;
         return three;
     }
 }
-int main(){
-    triple goal,one,two,three;
-    cin>>goal.a>>goal.b>>goal.c>>one.a>>one.b>>one.c>>two.a>>two.b>>two.c>>three.a>>three.b>>three.c;
-    
+int main() {
+    triple goal, one, two, three;
+    cin >> goal.a >> goal.b >> goal.c >> one.a >> one.b >> one.c >> two.a >> two.b >> two.c >> three.a >> three.b >> three.c;
+
 }
