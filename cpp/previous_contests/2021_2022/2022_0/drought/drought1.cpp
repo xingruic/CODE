25fc1097eded4848a7cff21f8db06b64681fc0e0
NevGithub0823 <xingruichen2@gmail.com>
2022-03-06 18:37:45 -0800

    usaco drought&herdle

--- cpp/previous_contests/2021_2022/2022_0/drought/drought1.cpp
+++ cpp/previous_contests/2021_2022/2022_0/drought/drought1.cpp
@@ -1,49 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int t;
-int num[101];
-vector<long long> cows[101];
-int dp[936558]; // 936557 is a prime
-int hash2cow[936558];
-int _hash(vector<long long> v) {
-    int sum = 1;
-    for (int i = 0; i < v.size(); i++) {
-        sum = sum * (v[i] + 1) + v[i];
-    }
-    return sum % 936557;
-}
+int a[100001];
+int b[100001];
+int ans[101];
 int main() {
-    memset(dp, -1, sizeof(dp));
-    memset(hash2cow, -1, sizeof(hash2cow));
+    memset(ans, -1, sizeof(ans));
     cin >> t;
-    long long tmp;
     for (int i = 1; i <= t; i++) {
-        cin >> num[i];
-        for (int j = 1; j <= num[i]; j++) {
-            cin >> tmp;
-            cows[i].push_back(tmp);
+        memset(a, 0, sizeof(a));
+        int n;
+        cin >> n;
+        for (int j = 1; j <= n; j++) {
+            cin >> a[j];
         }
+        if (n > 1 && (a[1] > a[2] || a[n] > a[n - 1])) {
+            continue;
+        }
+        int diff = 0; // difference between odd and even digits
+        int _min = 0x7fffffff;
+        bool f = 1;
+        for (int j = 1; j <= n; j++) {
+            _min = min(_min, a[j]);
+            // if (f)
+            //     diff += a[j];
+            // else
+            //     diff -= a[j];
+            b[j] = a[j];
+        }
+        for (int k = _min; k >= 0; k--) {
+            for (int j = 1; j < n; j++) {
+                while (b[j] > k && b[j + 1] > k)
+                    b[j]--, b[j + 1]--;
+            }
+            f = 0;
+            for (int j = 1; j <= n; j++) {
+                if (b[j] != k) {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f)
+                continue;
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                cnt += a[j] - k;
+            }
+            ans[i] = cnt;
+            break;
+        }
+        // ans[i] is defaulted to -1, so no need to set it here
     }
-    int ans = 0x7fffffff;
     for (int i = 1; i <= t; i++) {
-        vector<long long> tmp = cows[i];
-        int t = _hash(cows[i]);
-        for (int j = 0; j < cows[i].size() - 1; j++) {
-            if (tmp[j] > 0 && tmp[j + 1] > 0) {
-                tmp[j]++, tmp[j + 1]++;
-                int tt = _hash(tmp);
-                dp[t] = dp[tt] + 2;
-            }
-        }
-        bool flag = 1;
-        for (int j = 0; j < cows[i].size() - 1; j++) {
-            if (cows[i][j] != cows[i][j + 1]) {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag) ans = min(ans, dp[t]);
-        ans = 0x7fffffff;
-        memset(dp, -1, sizeof(dp));
+        cout << ans[i] << endl;
     }
 }
